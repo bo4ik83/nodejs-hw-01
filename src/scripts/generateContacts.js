6c90fc8194ac797b103b10a2d2f758b73a86312d
Denis Bochev <denisbo4ev@gmail.com>
2025-02-19 19:33:59 +0200

    hw1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,20 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact';
+import { readContacts } from '../utils/readContacts';
+import { writeContacts } from '../utils/writeContacts';
+
+const generateContacts = async (number) => {
+  try {
+    const existingContacts = await readContacts();
+    const newContacts = Array.from({ lenght: number }, () =>
+      createFakeContact(),
+    );
+    const updatedContacts = [...existingContacts, ...newContacts];
+    await writeContacts(updatedContacts);
+
+    console.log('✅ Успішно додано ${number} контактів!');
+  } catch (error) {
+    console.error('❌ Помилка генерації контактів:', error.message);
+  }
+};
 
 generateContacts(5);
