61c02d36a133a5fb1e33e12156464dd6e14ef6da
Denis Bochev <denisbo4ev@gmail.com>
2025-02-19 20:01:02 +0200

    add contacts count

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,16 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
 
-console.log(await countContacts());
+export const countContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    const contactCount = contacts.length;
+    return contactCount;
+  } catch (error) {
+    console.error('❌ Помилка при зчитуванні контактів:', error.message);
+    return 0;
+  }
+};
+
+countContacts().then((contactCount) =>
+  console.log('✅ Кількість контактів:', contactCount),
+);
